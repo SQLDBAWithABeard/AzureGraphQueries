69e8725dcafe957d5f0a8f34ede2962cf845e375
Rob Sewell <mrrobsewell@outlook.com>
2022-05-18 09:42:10 +0100

    Update example queries.sql

--- example queries.sql
+++ example queries.sql
@@ -1,10 +1,35 @@
 // get me a list of resource, resource group and the resource tags and the resourcegroup tags
+
+$query = "
 resources
 | join kind=inner (
     resourcecontainers
     | where type =~ 'microsoft.resources/subscriptions/resourcegroups'
+    //| where tags['billing'] == ''
     | project subscriptionId, resourceGroup,rgtags=tags)
 on subscriptionId, resourceGroup
-|limit 10
+//|limit 10
 | project-away subscriptionId1, resourceGroup1
-| project name, type, subscriptionId,resourceGroup,resourcebilling=tags['billing'],rgbilling=rgtags['billing']
+| project subscriptionId,resourceGroup, name,rgbilling=rgtags['billing'], resourcebilling=tags['billing'], type
+"
+
+$date = Get-Date -Format yyyyMMdd-HHmmss
+$filename = './output/rgsandtags-{0}.xlsx' -f $date
+
+
+$pageSize = 1000
+$iteration = 0
+$searchParams = @{
+    Query = $query
+    First = $pageSize
+}
+
+$results = do {
+    $iteration += 1
+    Write-Verbose "Iteration #$iteration" -Verbose
+    $pageResults = Search-AzGraph @searchParams
+    $searchParams.Skip += $pageResults.Count
+    $pageResults
+} while ($pageResults.Count -eq $pageSize)
+$results.Count
+$results | Export-Excel -Path $filename -WorksheetName 'Rgs and Tags' -AutoSize -AutoFilter
